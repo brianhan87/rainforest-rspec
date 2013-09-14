b8cb4ff2cb21724e369ff3bbf977011775577d67
Brian Han <brianhan87usa@gmail.com>
2013-09-13 21:29:45 -0400

    Write specs for product.name presence validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have a name" do
+    product = Product.new
+    product.name.should == nil
+    product.name.should_not == "something"
+    product.name.should_not be_valid
+  end
 end
