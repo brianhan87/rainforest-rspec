02e8a3fdeaa7edc9527acaece972e1223e9b924b
Brian Han <brianhan87usa@gmail.com>
2013-09-16 13:19:01 -0400

    Add spec for product.price; Refactor product_spec.rb to use let(:product)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
 describe Product do
+  let(:product) { Product.new }
   it "should have a name" do
-    product = Product.new
     product.name.should == nil
     # product.name.should_not == "something"
     # product.name.should_not be_valid
   end
 
   it "should have a description" do
-    product = Product.new
     product.description.should == nil
+  end
+
+  it "should have a price" do
+    product.price.should == nil
   end
 
 end
