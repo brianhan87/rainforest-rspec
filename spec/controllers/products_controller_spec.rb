c56d4170a72e717e7bebf2004c853553cf5a47a1
Brian Han <brianhan87usa@gmail.com>
2013-09-17 13:17:21 -0400

    Set up routes/controller actions/views for index and new

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,7 +1,7 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe ProductsController do
-  describe 'GET /products' do
-    let!(:resources) { FactoryGirl.create :products}
-  end
-end
+# describe ProductsController do
+#   describe 'GET /products' do
+#     let!(:resources) { FactoryGirl.create :products}
+#   end
+# end
